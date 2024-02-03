1658d66a0b3183ee5674378f99907c331c9d10fb
addiesallow <a.derkacz@gmail.com>
2024-02-04 00:32:54 +0100

    Updated the file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  This project is licensed under the ${data.license} license.
+  
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+${data.tests}
+
+## Questions
+For questions, please contact me at ${data.email}.
+GitHub: [${data.github}](https://github.com/${data.github})
 `;
 }
 
