5ba081828d0bddcc5a614299f790916030ef6ed0
addiesallow <a.derkacz@gmail.com>
2024-02-04 00:59:49 +0100

    Added a question

--- index.js
+++ index.js
@@ -43,6 +43,11 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?,
+    },
+    {
+        type: 'input',
         name: 'email',
         message: 'Enter your email address.',
     }
